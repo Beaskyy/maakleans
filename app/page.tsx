886b782d41f14d146c2fe40c2a6e18415f3558d3
Beaskyy <afeezakintobi@nqb8.co>
2023-12-28 20:31:41 +0100

    header

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
+import Header from '@/components/header'
 import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center">
-      <h1 className='text-5xl font-bold'>Welcome to Maakleans Dry Cleaners Home page</h1>
+    <main>
+      <Header />
     </main>
   )
 }
