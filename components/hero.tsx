1b8d57bb57ef5eeb81ee7de4466b24ab885bb5bd
Beaskyy <afeezakintobi@nqb8.co>
2023-12-29 21:03:57 +0100

    sidebar

--- components/hero.tsx
+++ components/hero.tsx
@@ -3,9 +3,9 @@ import React from "react";
 
 const Hero = () => {
   return (
-    <div className="grid grid-cols-1 lg:grid-cols-2 p-20">
+    <div className="grid grid-cols-1 lg:grid-cols-2 p-4 lg:p-20 bg-sky-100">
       <div className="mt-20">
-        <h1 className="text-7xl font-bold mb-4">Lagos <br /> First Choice in Dry Cleaning</h1>
+        <h1 className="text-5xl lg:text-7xl font-bold mb-4">Lagos <br /> First Choice in Dry Cleaning</h1>
         <p className="max-w-md font-normal">A tradition of unsurpassed customer service and unwavering commitment to high quality dry cleaning and laundry service for over 20 years</p>
         <button className="mt-8">Sign up for free pickup and delivery</button>
       </div>
